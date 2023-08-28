53ec8370b94c011d05ab820747308ab71b79b550
Aimeric Marchau <marchauaimeric@gmail.com>
2023-08-28 17:45:17 +0200

    feat: refacto js function to save editable content

--- app/javascript/controllers/editing_controller.js
+++ app/javascript/controllers/editing_controller.js
@@ -2,50 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="editing"
 export default class extends Controller {
-  static targets = [ "checklistData", "sectionData", "stepData" ]
+  static targets = [ "item" ]
 
-  saveChecklist() {
-    const newText = this.checklistDataTarget.innerText
-    const checklistId = this.checklistDataTarget.dataset.checklistId
-    const dataType = this.checklistDataTarget.dataset.type
+  save() {
+    const newText = this.itemTarget.innerText
+    const itemId = this.itemTarget.dataset.id
+    const itemType = this.itemTarget.dataset.type
+    const route = this.itemTarget.dataset.route
 
-    fetch(`/checklists/${checklistId}`, {
+    fetch(`/${route}/${itemId}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').getAttribute('content')
       },
-      body: JSON.stringify({ [dataType]: newText }),
-    })
-  }
-
-  saveSection() {
-    const newText = this.sectionDataTarget.innerText
-    const sectionId = this.sectionDataTarget.dataset.sectionId
-    const dataType = this.sectionDataTarget.dataset.type
-
-    fetch(`/sections/${sectionId}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').getAttribute('content')
-      },
-      body: JSON.stringify({ [dataType]: newText }),
-    })
-  }
-
-  saveStep() {
-    const newText = this.stepDataTarget.innerText
-    const stepId = this.stepDataTarget.dataset.stepId
-    const dataType = this.stepDataTarget.dataset.type
-
-    fetch(`/steps/${stepId}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').getAttribute('content')
-      },
-      body: JSON.stringify({ [dataType]: newText }),
+      body: JSON.stringify({ [itemType]: newText }),
     })
   }
 }
