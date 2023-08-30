390df58472596bf8f34f2a896d14c5cdc26e6189
Aimeric Marchau <marchauaimeric@gmail.com>
2023-08-30 11:28:06 +0200

    build: added acts_as_list gem and updated models + seed

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,9 @@
 class Section < ApplicationRecord
   belongs_to :checklist
   has_many :steps, dependent: :destroy
+  acts_as_list scope: :checklist
 
   validates :title, presence: true
   validates :checklist, presence: true
-
+  validates :position, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@ puts '🌱 Creating users...'
 User.create!(first_name: 'Aimeric',
              last_name: 'Marchau',
              email: 'marchauaimeric@gmail.com',
-             password: 'azerty'
-)
+             password: 'azerty')
+
 User.create!(first_name: 'Stéphanie',
              last_name: 'Maley',
              email: 'mariestephanie.maley@gmail.com',
-             password: 'azerty'
-)
+             password: 'azerty')
+
 puts '🌱 Users created!'
 
 puts '💼 Creating workspaces...'
@@ -29,50 +29,50 @@ Checklist.create!(name: 'Checklist for a successful project',
                   workspace: User.first.workspaces.first,
                   description: 'This checklist is a reminder of all the steps to follow to ensure the success of a project.',
                   start_date: Date.new(2023, 9, 1),
-                  estimated_end_date: Date.new(2023, 9, 30),
-)
+                  estimated_end_date: Date.new(2023, 9, 30))
+
 Checklist.create!(name: 'Malt onboarding',
                   workspace: User.first.workspaces.first,
                   description: 'All the steps to create a Malt account and start working as a freelancer.',
                   start_date: Date.new(2023, 9, 6),
-                  estimated_end_date: Date.new(2023, 9, 22),
-)
+                  estimated_end_date: Date.new(2023, 9, 22))
+
 Checklist.create!(name: 'Project onboarding',
                   workspace: User.first.workspaces.second,
                   description: 'The steps you must follow so I can start working on your project.',
                   start_date: Date.new(2023, 8, 15),
-                  estimated_end_date: Date.new(2023, 8, 31),
-)
+                  estimated_end_date: Date.new(2023, 8, 31))
+
 Checklist.create!(name: 'Flat search',
                   workspace: User.second.workspaces.first,
                   description: 'To do to find a flat',
                   start_date: Date.new(2023, 8, 20),
-                  estimated_end_date: Date.new(2023, 9, 12),
-)
+                  estimated_end_date: Date.new(2023, 9, 12))
+
 Checklist.create!(name: 'Webflow tutorial',
                   workspace: User.second.workspaces.second,
                   description: 'How to create a website with Webflow',
                   start_date: Date.new(2023, 9, 15),
-                  estimated_end_date: Date.new(2023, 10, 15),
-)
+                  estimated_end_date: Date.new(2023, 10, 15))
+
 Checklist.create!(name: 'Slack tutorial',
                   workspace: User.second.workspaces.second,
                   description: 'How to create a Slack channel',
                   start_date: Date.new(2023, 10, 1),
-                  estimated_end_date: Date.new(2023, 10, 15),
-)
+                  estimated_end_date: Date.new(2023, 10, 15))
+
 puts '📝 Checklists created!'
 
 puts '🚩 Creating sections...'
 Checklist.all.each do |checklist|
-  Section.create!(title: 'First section', checklist: checklist)
-  Section.create!(title: 'Second section', checklist: checklist)
+  Section.create!(title: 'First section', checklist: checklist, position: 1)
+  Section.create!(title: 'Second section', checklist: checklist, position: 2)
 end
 puts '🚩 Sections created!'
 
 puts '📌 Creating steps...'
 Section.all.each do |section|
-  Step.create!(title: 'First step', section: section, completed: true)
-  Step.create!(title: 'Second step', section: section, completed: false)
+  Step.create!(title: 'First step', section: section, completed: true, position: 1)
+  Step.create!(title: 'Second step', section: section, completed: false, position: 2)
 end
 puts '📌 Steps created!'
