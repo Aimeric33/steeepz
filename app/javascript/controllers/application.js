2ab2257647d5bf87820395ab0c0a35f82416a8b3
Aimeric Marchau <marchauaimeric@gmail.com>
2023-09-17 23:03:01 +0200

    feat: able to delete a workspace

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import Dropdown from "stimulus-dropdown"
 
 const application = Application.start()
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+Stimulus.register("dropdown", Dropdown)
 
 export { application }
